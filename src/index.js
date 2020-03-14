bb7510424d7f8f8a827c833ade23cf86cf7c042d
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2020-03-14 14:16:50 +0300

    feat: add small part of solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,59 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let bracketCount = 0;
+    
+    // brackets counter
+    for(let i = 0; i < expr.length; i++){
+        if(expr[i] == '('){
+            bracketCount++;
+        }
+        else if(expr[i] == ')'){
+            bracketCount--;
+        }
+    }
+    if(bracketCount !== 0) throw new Error ('ExpressionError: Brackets must be paired');
+
+
+    
+    for(let j = 0; j < expr.length; j++){
+        // simple addition
+        if(expr[j] == '+'){
+	        let numbersString = expr.split('+');
+	        let numbers = numbersString.map(a => Number(a));
+	        let reducer = (accumulator, currentValue) => accumulator + currentValue;
+            let result = numbers.reduce(reducer);
+	        return result;
+        } 
+        // simple subtraction
+        else if(expr[j] == '-'){
+            let numbersString = expr.split('-');
+	        let numbers = numbersString.map(a => Number(a));
+	        let reducer = (accumulator, currentValue) => accumulator - currentValue;
+            let result = numbers.reduce(reducer);
+	        return result;
+        }
+        // simple division
+        else if(expr[j] == '/'){
+            let numbersString = expr.split('/');
+	        let numbers = numbersString.map(a => Number(a));
+	        let reducer = (accumulator, currentValue) => accumulator / currentValue;
+            let result = numbers.reduce(reducer);
+            // division by zero
+            if(result === Infinity){
+                throw new Error("TypeError: Division by zero.");
+              }
+	        return result;
+        }
+        // simple multiplication
+        else if(expr[j] == '*'){
+            let numbersString = expr.split('*');
+	        let numbers = numbersString.map(a => Number(a));
+	        let reducer = (accumulator, currentValue) => accumulator * currentValue;
+            let result = numbers.reduce(reducer);
+	        return result;
+        }
+    }
 }
 
 module.exports = {
