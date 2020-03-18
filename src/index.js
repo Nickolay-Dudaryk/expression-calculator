2cd1a59f7c87194ef883763929d672039f4c4662
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2020-03-18 23:45:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,60 +3,77 @@ function eval() {
     return;
 }
 
+
 function expressionCalculator(expr) {
-    let bracketCount = 0;
-    
-    // brackets counter
-    for(let i = 0; i < expr.length; i++){
-        if(expr[i] == '('){
-            bracketCount++;
+    expr = expr.replace(/\s/g, '').replace(/(\*|\/|\+|\-)/g, ' $& ');
+    //   expr.replace(/\s/g, '') - remove all found white spaces in expression
+    //   .replace(/(\*|\/|\+|\-)/g, ' $& ') - add white spaces around math signs
+    let openBrackets = 0,
+        closeBrackets = 0;
+
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === '(') {
+            openBrackets += 1;
         }
-        else if(expr[i] == ')'){
-            bracketCount--;
+        if (expr[i] == ')') {
+            closeBrackets += 1;
         }
     }
-    if(bracketCount !== 0) throw new Error ('ExpressionError: Brackets must be paired');
+    if (openBrackets !== closeBrackets) {
+        throw new Error("ExpressionError: Brackets must be paired.");
+    }
 
+    let expressionWithBrackets;
 
-    
-    for(let j = 0; j < expr.length; j++){
-        // simple addition
-        if(expr[j] == '+'){
-	        let numbersString = expr.split('+');
-	        let numbers = numbersString.map(a => Number(a));
-	        let reducer = (accumulator, currentValue) => accumulator + currentValue;
-            let result = numbers.reduce(reducer);
-	        return result;
-        } 
-        // simple subtraction
-        else if(expr[j] == '-'){
-            let numbersString = expr.split('-');
-	        let numbers = numbersString.map(a => Number(a));
-	        let reducer = (accumulator, currentValue) => accumulator - currentValue;
-            let result = numbers.reduce(reducer);
-	        return result;
+    while (openBrackets > 0) {
+        if ((expressionWithBrackets = expr.match(/(\([0-9\+\/\*\-. ]+\))/g)) !== null) { 
+
+            for (let i = 0; i < expressionWithBrackets.length; i++) {
+                let exprInBrackets = expressionWithBrackets[i].replace('(', '').replace(')', '');
+                expr = expr.replace(expressionWithBrackets[i], calc(exprInBrackets));
+            }
         }
-        // simple division
-        else if(expr[j] == '/'){
-            let numbersString = expr.split('/');
-	        let numbers = numbersString.map(a => Number(a));
-	        let reducer = (accumulator, currentValue) => accumulator / currentValue;
-            let result = numbers.reduce(reducer);
-            // division by zero
-            if(result === Infinity){
-                throw new Error("TypeError: Division by zero.");
-              }
-	        return result;
+        openBrackets -= 1;
+    }
+    return calc(expr);
+}
+
+function calc(expr) {
+    let arr = expr.split(' ');
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "*") {
+            arr[i] = arr[i - 1] * arr[i + 1];
+            arr.splice(i - 1, 1);
+            arr.splice(i, 1);
+            i--;
         }
-        // simple multiplication
-        else if(expr[j] == '*'){
-            let numbersString = expr.split('*');
-	        let numbers = numbersString.map(a => Number(a));
-	        let reducer = (accumulator, currentValue) => accumulator * currentValue;
-            let result = numbers.reduce(reducer);
-	        return result;
+        if (arr[i] === "/") {
+            if (arr[i + 1] === '0'){
+                throw new TypeError('TypeError: Division by zero.');
+            } 
+            arr[i] = arr[i - 1] / arr[i + 1];
+            arr.splice(i - 1, 1);
+            arr.splice(i, 1);
+            i--;
         }
     }
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "+") {
+            arr[i] = Number(arr[i - 1]) + Number(arr[i + 1]);
+            arr.splice(i - 1, 1);
+            arr.splice(i, 1);
+            i--;
+        }
+        if (arr[i] === "-") {
+            arr[i] = arr[i - 1] - arr[i + 1];
+            arr.splice(i - 1, 1);
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    return Number(arr);
 }
 
 module.exports = {
